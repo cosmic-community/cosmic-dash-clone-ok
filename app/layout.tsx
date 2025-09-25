ea4c6093a4afc541586147269949a0359b28be2a
Cosmic AI <support@cosmicjs.com>
2025-09-25 02:00:58 -0700

    edit app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,31 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        {/* GTM bootstrap (after hydration so it doesn't block) */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
+        {/* GTM <noscript> for no-JS users (safe in body) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <Header />
-        <main className="flex-grow">
-          {children}
-        </main>
+        <main className="flex-grow">{children}</main>
         <Footer />
         <CosmicBadge bucketSlug={bucketSlug} />
       </body>
